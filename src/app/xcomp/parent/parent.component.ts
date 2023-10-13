7f773009a59706ec0bd0f3dfeec8513b65f681ea
owermaiden <ower.maiden@cybertekschool.com>
2023-10-13 10:27:05 -0400

    xcomp

--- src/app/xcomp/parent/parent.component.ts
+++ src/app/xcomp/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Mentor } from 'src/app/shared/mentor';
 
 @Component({
   selector: 'app-parent',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ParentComponent {
 
+  mentors: Mentor[] = [
+    new Mentor(1,'Ower', 12),
+    new Mentor(2,'Sahin', 23),
+    new Mentor(3,'Matt', 4)
+  ];
+
+  selected: Mentor | undefined;
+
+  onSelect(mentor: Mentor){
+    this.selected = mentor;
+    console.log(mentor.name)
+  }
 }
