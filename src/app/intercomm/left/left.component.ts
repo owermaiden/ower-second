1a0f7c4d34eae553329b8a3736d585d8d4660f93
owermaiden <ower.maiden@cybertekschool.com>
2023-10-16 10:29:00 -0400

    html

--- src/app/intercomm/left/left.component.ts
+++ src/app/intercomm/left/left.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MentorService } from 'src/app/mentor.service';
+import { Mentor } from 'src/app/shared/mentor';
 
 @Component({
   selector: 'app-left',
   templateUrl: './left.component.html',
   styleUrls: ['./left.component.css']
 })
-export class LeftComponent {
+export class LeftComponent implements OnInit {
+
+
+  mentors: Mentor[] = [];
+
+  constructor(private service: MentorService){}
+
+  ngOnInit(): void {
+    this.mentors = this.service.mentors;
+  }
+
+  onSelectMentor(mentor: Mentor){
+    console.log(mentor);
+  }
 
 }
