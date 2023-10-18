96dacf946bee1948d7972683cf2bc312d52a7687
owermaiden <ower.maiden@cybertekschool.com>
2023-10-18 11:03:42 -0400

    mentor plus

--- src/app/mentor.service.ts
+++ src/app/mentor.service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Mentor } from './shared/mentor';
 import { HttpClient } from  '@angular/common/http';
+import { Observable, filter, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MentorService {
   
-  mentors: Mentor[] = [
-    new Mentor(1,'Ower', 12),
-    new Mentor(2,'Sahin', 23),
-    new Mentor(3,'Matt', 4)
-  ];
+  mentors: Mentor[] = [];
+
+  baseUrl:string = 'localhost:8080/api/v1/mentors';
 
   selectedMentor : Mentor = this.mentors[0];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  fetchMentor$():Observable<Mentor[]>{
+    return this.http.get<ResponseType>(this.baseUrl)
+            .pipe(
+              map(response => response.data), // Function
+            );
+  }
 
   likeMentor(liked: Mentor){
     let index = this.mentors.findIndex(mentor => mentor.id === liked.id);
     this.mentors[index]!.likeCount! += 1;
   }
+}
+
+interface ResponseType{
+  data: Mentor[]
 }
