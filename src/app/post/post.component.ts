359651a0729de0ca2f0457f138726aae7e478356
owermaiden <ower.maiden@cybertekschool.com>
2023-10-18 10:38:34 -0400

    rxjs

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../shared/post';
 import { PostService } from '../post.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -15,8 +16,20 @@ export class PostComponent implements OnInit{
 
   ngOnInit(): void {
     this.service.getPosts().subscribe(
-      response => this.posts = response
+      (response : Post[]) => this.posts = response
     )
   }
+
+  numbers : Observable<number> = of(1,2,3,4,5,6,7,8,9);  // numbers.stream().
+  
+  words: Observable<string> = new Observable<string>(
+    (observer) => {
+      observer.next("hello");
+      observer.next("world");
+      setTimeout(() => {
+        observer.next("Final")
+      },10000);
+    }
+  );
 
 }
